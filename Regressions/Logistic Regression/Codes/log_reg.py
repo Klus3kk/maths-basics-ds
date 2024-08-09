67914fc1d7972a1de6308af0bb8aa97f1af302f2
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-08-09 18:45:55 +0200

    Regressions

--- Regressions/Logistic Regression/Codes/log_reg.py
+++ Regressions/Logistic Regression/Codes/log_reg.py
@@ -1,3 +1,13 @@
 import pandas as pd 
 from sklearn.linear_model import LogisticRegression
 
+df = pd.read_csv("data.csv", delimiter=",")
+
+X = df.values[:, :-1]
+Y = df.values[:, -1]
+
+model = LogisticRegression(penalty=None)
+model.fit(X, Y)
+
+print(model.coef_.flatten())
+print(model.intercept_.flatten())
